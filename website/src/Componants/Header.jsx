9c1b39c72eac2ec5cef28c283321f869c02c9228
Shubham Singh <152617144+Noobdev28@users.noreply.github.com>
2024-02-15 20:08:29 +0530

    Update Header.jsx

--- website/src/Componants/Header.jsx
+++ website/src/Componants/Header.jsx
@@ -33,14 +33,14 @@ export default function Header() {
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <NavLink
+                           <Link
                                 to="/"
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b ${isActive ? "text-red-600" :"text-gray-600"} border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
-                                </NavLink>
+                           </Link>
                             </li>
                             <li>
                                 <NavLink
